7e7ee8673594b12092ffe3c56ee0394c59462fb8
yixian03 <yixian920309@gmail.com>
2023-06-18 13:06:57 +0800

    3rd_update

--- src/state/state.cpp
+++ src/state/state.cpp
@@ -14,7 +14,19 @@
  */
 int State::evaluate(){
   // [TODO] design your own evaluation function
-  return 0;
+  int score = 0;
+  for(int i=0;i<2;i++){
+    for(int j=0;j<BOARD_H;j++){
+      for(int k=0;k<BOARD_W;k++){
+        if(i == 0){ //white
+          score += 5 * this->board.board[i][j][k];
+        }else{  //black
+          score -= 5 * this->board.board[i][j][k];
+        }
+      }
+    }
+  }
+  return score;
 }
 
 
